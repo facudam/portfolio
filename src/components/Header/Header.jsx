0518544fce9b6530e4d2968229f2fcadf9594238
facudam <facundo89caceres@gmail.com>
2023-06-20 11:09:01 -0300

    updating linkedin url profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
                 <h1><span>Hi there! </span>I'm Facundo Cáceres</h1>
                 <p>A frontend React developer and SEO optimizer based in Bs As - Argentina; with a focus on web applications. I really love programming and development as much as I love playing guitars and composing music. </p>
                 <div className='main-header__navigation'>
-                    <ContactLink href='https://www.linkedin.com/in/facundo-c%C3%A1ceres/' src='https://icongr.am/devicon/linkedin-original.svg?size=83&color=000000' alt='linkedin logo' title='linkedin' />
+                    <ContactLink href='https://www.linkedin.com/in/facundo-caceres-dev/' src='https://icongr.am/devicon/linkedin-original.svg?size=83&color=000000' alt='linkedin logo' title='linkedin' />
                     <ContactLink href='mailto: facundocaceres.email@gmail.com' src={GMAIL} alt='gmail logo' title='email' />
                     <ContactLink href='https://github.com/facudam' src='https://icongr.am/devicon/github-original.svg?size=83&color=000000' alt='github logo' title='github'/>
                     <ContactLink href='https://twitter.com/FCaceres15' src='https://icongr.am/devicon/twitter-original.svg?size=83&color=currentColor' alt='twitter logo' title='twitter' />
