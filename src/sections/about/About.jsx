a4b74eca26fcc178c852f487f4cb8cc13f63d81c
facudam <facundo89caceres@gmail.com>
2023-09-09 13:45:16 -0300

    Modifying about section subtitle

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
             <div className='paragraph-ctn'>
                 <img src={ WEBDEV } alt='web development' />
                 <div className='about-section__texts'>
-                    <h3>How I start programming</h3>
+                    <h3>How I started programming</h3>
                     <p>Before diving into the world of programming, I was fascinated by search engine optimization (SEO) and its impact on website visibility and user engagement. I learned the ins and outs of optimizing websites for search engines and achieved excellent results while applying it to personal websites.</p>
                     <p>The SEO was the starting point for me to delve into the world of web programming.</p>
                     <p> Currently, I am pursuing my programming education at university while also being a self-taught learner. I thrive on continuously expanding my skill set and staying up to date with the latest industry trends.</p>
